16edddff1dea79c3a41a150731558efd517995c8
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-26 00:21:52 +0300

    Add project files.

---
+++ CSharp17/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+ArrayList liste = new ArrayList();
+
+liste.Add("Ali");
+liste.Add("Talu");
+liste.Add(23);
+liste.Add("E");
+
+foreach(var list in liste)
+{
+    Console.WriteLine(list);
+}
